228f32fdaf57df50be3c10659a7e57e34102373a
José Gaspar <34697686+josemgaspar@users.noreply.github.com>
2019-03-22 17:53:05 +0000

    Add files via upload
    Optimal Growth - Direct and indirect methods codes
    Requires GPOPS package.
    Add files and GPOPS folder to the same Matlab folder path

---
+++ Optimal growth - direct and indirect methods/ramseygpopsDae.m
@@ -0,0 +1,28 @@
+%-------------------------------------
+% BEGIN: function infHorizonDae.m
+%-------------------------------------
+function [dae Ddae]= ramseygpopsDae(sol)
+
+
+t = sol.time;
+k = sol.state;
+c = sol.control;
+p = sol.parameter;
+global alpha delta n gx
+
+%Restrição dinâmica
+kdot = k.^alpha-c-(delta+n+gx).*k;
+
+dae = (kdot);
+if nargout == 2
+dfdx = alpha.*k.^(alpha-1)-(delta+n+gx);
+dfdu = -ones(size(c));
+dfdt = zeros(size(t));
+dfdp = zeros(size(p));
+Ddae = [ dfdx dfdu dfdt dfdp];
+end
+% Ddae = [df/dx, df/du, df/dt, df/dp]
+%-----------------------------------
+% END: function infHorizonDae.m
+%-----------------------------------
+
